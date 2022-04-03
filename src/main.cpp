bfe425ea353359d1cff93b887604196c5dafd992
Mihail Dimitrov <36842438+bladedbicycle384@users.noreply.github.com>
2022-04-03 10:59:01 +0300

    Finalized version
    Left some of the testing methods, serial prints and file creation as comments to demonstrate how it was done, will remove in a later commit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
+#include <esp_camera.h>
 #include <ESP32Servo.h>
+#include <FS.h>
 #include <SPIFFS.h>
 #include <ESP32QRCodeReader.h>
 #include <BluetoothSerial.h>
@@ -12,16 +14,74 @@ ESP32QRCodeReader scanner(CAMERA_MODEL_AI_THINKER);
 BluetoothSerial lockBT;
 Servo lockServo;
 
-int authCount = 0;
+int authCount;
+int testCount = 1;
+String add = "add\r";
+String del = "delete\r";
+String stp = "stop\r";
+struct QRCodeData qrCode;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println();
+
+  SPIFFS.begin(true);
 
   scanner.setup();
   scanner.begin();
 
+  delay(5000);
+
+  // SPIFFS.remove("accesslist.txt");
+
+  // File root = SPIFFS.open("/");
+  // File file = root.openNextFile();
+  // Serial.println("showcasing files.");
+
+  // while(file)
+  // {
+  //   Serial.print("FILE: ");
+  //   Serial.println(file.name());
+  //   file = root.openNextFile();
+  // }
+
+  // File authList = SPIFFS.open("/accesslist.txt", FILE_WRITE);
+  // if(!authList)
+  // {
+  //   Serial.println("error opening file");
+  //   return;
+  // }
+  // if(authList.println("masterkey"))
+  // {
+  //   Serial.println("File was written");
+  // }
+  // else
+  // {
+  //   Serial.println("file write failed");
+  // }
+  // if(authList.println("testkey1"))
+  // {
+  //   Serial.println("File was written");
+  // }
+  // else
+  // {
+  //   Serial.println("file write failed");
+  // }
+  // if(authList.println("test key 2"))
+  // {
+  //   Serial.println("File was written");
+  // }
+  // else
+  // {
+  //   Serial.println("file write failed");
+  // }
+  // authList.close();
+
+
+  // while(authList.available())
+  // {
+  //   Serial.write(authList.read());
+  // }
 
   lockServo.attach(LOCK_PIN);
 }
@@ -33,35 +93,67 @@ void closeLock()
 
 void openLock()
 {
-  lockServo.write(180);
+  lockServo.write(90);
 }
 
 void QRScan()
 {
-  struct QRCodeData qrCode;
+  Serial.println("before opening file");
 
-  File authList = SPIFFS.open("/access_list.txt", "r");
+  File authList = SPIFFS.open("/accesslist.txt");
   if(!authList)
   {
     Serial.println("There was an error opening the access list\n");
     return;
   }
+  Serial.println("Scanning for qr");
+
   vector<String> authVect;
   while(authList.available())
   {
     authVect.push_back(authList.readStringUntil('\n'));
   }
   authList.close();
-  
+
+  // for(String s : authVect)
+  // {
+  //   Serial.println(s);
+  // }
+
+  Serial.println(testCount);
+  Serial.println("");
+  testCount++;
+  delay(2000);
+
   if(scanner.receiveQrCode(&qrCode, 100))
   {
+    Serial.print("QR scanned ");
+    String qrData = (const char*)qrCode.payload;
+    qrData += '\r';
     if(qrCode.valid)
     {
-      if(strcmp((char*)qrCode.payload, authVect[0].c_str()) == 0)
+      Serial.print("Valid: ");
+      Serial.println(qrData);
+
+  	  // for(String s: authVect)
+      // {
+      //   Serial.println(s);
+      //   Serial.println(qrData);
+      //   if(s == qrData)
+      //   {
+      //     Serial.println("identical!?");
+      //   }
+      //   else
+      //   {
+      //     Serial.println("sadly not :pensive:");
+      //   }
+      // }
+
+      if(qrData == authVect[0])
       {
         Serial.println("Entered master key code, enabling bluetooth for editing\n");
         lockBT.begin("SmartLock");
-        File adminAccessList = SPIFFS.open("/access_list.txt", FILE_WRITE);
+        File adminAccessList = SPIFFS.open("/accesslist.txt", FILE_WRITE);
         if(!adminAccessList)
         {
           Serial.println("There was an error opening the access list\n");
@@ -71,12 +163,14 @@ void QRScan()
         while(true)
         {
           String btData = lockBT.readString();
-          if(strcmp("add", btData.c_str()) == 0)
+          btData += '\r';
+          if(add == btData)
           {
             while(true)
             {
               String newAuthData = lockBT.readString();
-              if(strcmp("stop", newAuthData.c_str()) == 0)
+              newAuthData += '\r';
+              if(stp == newAuthData)
               {
                 lockBT.println("Exited addition mode");
                 break;
@@ -89,7 +183,7 @@ void QRScan()
               }
             }
           }
-          else if(strcmp("delete", btData.c_str()) == 0)
+          else if(del == btData)
           {
             while(true)
             {
@@ -97,23 +191,24 @@ void QRScan()
               bool found = true;
 
               String delAuthData = lockBT.readString();
+              delAuthData += '\r';
               if(delAuthData.length() != 0)
               {
                 for(int i = 0; i < authCount; i++)
                 {
-                  if(strcmp("stop", delAuthData.c_str()) == 0)
+                  if(stp == delAuthData)
                   {
                     stopped = true;
                     break;
                   }
-                  else if(strcmp(delAuthData.c_str(), authVect[i].c_str()) == 0)
+                  else if(delAuthData == authVect[i])
                   {
                     adminAccessList.seek(SeekSet);
                     uint8_t delSize = authVect[i].length() + 1;
                     for(int x= 0; x < authCount; x++)
                     {
                       String testDel = adminAccessList.readStringUntil('\n');
-                      if(strcmp(authVect[i].c_str(), testDel.c_str()) == 0)
+                      if(authVect[i] = testDel)
                       {
                         adminAccessList.seek(-testDel.length(), SeekCur);
                         break;
@@ -148,7 +243,7 @@ void QRScan()
               }
             }
           }
-          else if(strcmp("stop", btData.c_str()) == 0)
+          else if(stp = btData)
           {
             lockBT.println("Exiting admin mode\n");
             adminAccessList.close();
@@ -162,18 +257,19 @@ void QRScan()
         int i = 0;
         while(true)
         {
-          if(i = authCount)
+          if(i == authCount)
           {
             Serial.println("Access denied, no such key\n");
-            return;
+            break;
           }
-          if(strcmp((const char*)qrCode.payload, authVect[i].c_str()) == 0)
+          if(qrData == authVect[i])
           {
             Serial.println("QR key accepted, opening lock\n");
             openLock();
             delay(10000);
+            Serial.println("Closing lock");
             closeLock();
-            return;
+            break;
           }
           i++;
         }
@@ -189,6 +285,32 @@ void QRScan()
 
 void loop()
 {
+  authCount = 0;
+  File authCountF = SPIFFS.open("/accesslist.txt");
+  vector<String> authCountV;
+  while(authCountF.available())
+  {
+    authCountV.push_back(authCountF.readStringUntil('\n'));
+    authCount++;
+    Serial.println(authCount);
+  }
+
   QRScan();
-  delay(1000);
+  // String tk1 = "testkey1\r";
+  
+  // for(String s : authVect)
+  // {
+  //   Serial.println(s);
+  //   Serial.println(tk1);
+  //   if(s == tk1)
+  //   {
+  //     Serial.println("identical!?");
+  //   }
+  //   else
+  //   {
+  //     Serial.println("sadly not :pensive:");
+  //   }
+  // }
+  authCountF.close();
+  delay(2500);
 }
